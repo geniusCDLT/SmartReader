d9412c60ebb08696128c70e2ae5db0f7795995ad
carollkarry <carollkarry@cug.edu.cn>
2022-12-16 22:31:38 +0800

    小说实体类与章节实体类

---
+++ app/src/main/java/com/example/smartreader/entity/Chapter.java
@@ -0,0 +1,50 @@
+package com.example.smartreader.entity;
+
+import java.sql.Time;
+
+//小说章节实体类
+public class Chapter {
+    private Integer id;
+    private String title;
+    private Integer novelId;
+    private Time cTime;//创建时间
+    private Time mTime;//修改时间
+    private String content;//章节内容
+
+    public Integer getId(){
+        return this.id;
+    }
+    public void setId(Integer id){
+        this.id=id;
+    }
+    public String getTitle(){
+        return this.title;
+    }
+    public void setTitle(String title){
+        this.title=title;
+    }
+    public Integer getNovelId(){
+        return this.novelId;
+    }
+    public void setNovelId(Integer novelId){
+        this.novelId=novelId;
+    }
+    public Time getcTime(){
+        return this.cTime;
+    }
+    public void setcTime(Time cTime){
+        this.cTime=cTime;
+    }
+    public Time getmTime(){
+        return this.mTime;
+    }
+    public void setmTime(Time mTime){
+        this.mTime=mTime;
+    }
+    public String getContent(){
+        return this.content;
+    }
+    public void setContent(String content){
+        this.content=content;
+    }
+}
