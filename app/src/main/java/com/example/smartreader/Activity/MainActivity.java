a4d3c3edf06bf5285f93f72a9e0fd01d75f0e57f
lbrmerix <363127772@qq.com>
2022-12-20 21:57:12 +0800

    主界面滑动栏（待完善）以及阅读界面

--- app/src/main/java/com/example/smartreader/Activity/MainActivity.java
+++ app/src/main/java/com/example/smartreader/Activity/MainActivity.java
@@ -6,15 +6,38 @@ import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import com.example.smartreader.Activity.fragment.BookMallFragment;
+import com.example.smartreader.Activity.fragment.BookshelfFragment;
 import com.example.smartreader.R;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private TextView register;
+    /***************Object*********************/
+    private final ArrayList<Fragment> mFragmentList = new ArrayList<>();
+
+    protected List<Fragment> createTabFragments() {
+        initFragment();
+        return mFragmentList;
+    }
+
+    private void initFragment() {
+
+        Fragment BookshelfFragment = new BookshelfFragment();
+        Fragment BookMallFragment = new BookMallFragment();
+        mFragmentList.add(BookshelfFragment);
+        mFragmentList.add(BookMallFragment);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initFragment();
         setContentView(R.layout.activity_main);
 
     }
